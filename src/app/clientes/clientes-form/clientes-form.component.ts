cf044532e4328ce82ea8f191d5a90aad80047384
Gustavo Moreira <gustavo.ms@protonmail.com>
2021-10-23 18:16:18 -0300

    em andamento form-clientes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,3 +1,4 @@
+import { Cliente } from './../cliente';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesFormComponent implements OnInit {
 
-  constructor() { }
+  cliente : Cliente;
+
+  constructor() {
+    this.cliente = new Cliente();
+  }
 
   ngOnInit(): void {
+    //console.log(this.cliente.nome)
+  }
+
+  adicionar(){
+    console.log(this.cliente.nome)
   }
 
 }
